7561d20b8c999a53fad88874ca0763f31a7c472b
Lucas Viana <lucasvfv@gmail.com>
2023-04-14 15:42:00 -0300

    feat: share feature added

--- src/components/Form/ResultIMC/index.js
+++ src/components/Form/ResultIMC/index.js
@@ -1,9 +1,31 @@
-import { Text, View } from "react-native";
+import {
+	Text,
+	View,
+	TouchableOpacity,
+	Share
+ } from "react-native";
 import styles from "./styles";
 
 export default function ResultIMC(props){
+
+	const onShare = async () => {
+		const result = await Share.share({
+			message: "Meu imc hoje é: " + props.resultIMC
+		})
+
+	}
+
 	return (
 		<View style={styles.resultIMC}>
+			<View style={styles.shareBox}> 
+				{
+					props.resultIMC ?  
+					<TouchableOpacity style={styles.share} onPress={onShare}>
+						<Text style={styles.shareText}>Share</Text>
+					</TouchableOpacity>
+					: <View/>
+				}
+			</View>
 			<Text style={styles.resultInfo}>{props.messageResultIMC}</Text>
 			<Text style={styles.resultNumber}>{props.resultIMC}</Text>
 		</View>
--- src/components/Form/ResultIMC/styles.js
+++ src/components/Form/ResultIMC/styles.js
@@ -19,6 +19,22 @@ const styles = StyleSheet.create({
 		color:'#ff0043',
 		fontWeight: 'bold',
 		fontSize: 48,
+	},
+	shareBox: {
+		width:'100%',
+		alignItems: 'center',
+		marginBottom: 10,
+	},
+	share: {
+		backgroundColor: '#1877f2',
+		borderRadius: 50,
+		paddingBottom: 5,
+		paddingTop: 5,
+	},
+	shareText: {
+		color: '#fff',
+		fontWeight: 'bold',
+		paddingHorizontal: 30
 	}
 })
 
